47f97196e3e07db5110398e6f4d5a2a227eb6c36
Truong Dang <dangtruong080593@gmail.com>
2022-08-11 23:54:18 +0700

    Add files via upload

---
+++ TestCVF.f90
@@ -0,0 +1,30 @@
+!  TestCVF.f90 
+!
+!  FUNCTIONS:
+!	TestCVF      - Entry point of console application.
+!
+!	
+!
+
+!****************************************************************************
+!
+!  PROGRAM: TestCVF
+!
+!  PURPOSE: Test Compaq Visual Fortran with derived type, pointer etc.
+!
+!****************************************************************************
+
+	program TestCVF
+
+	use mdl_linked_list
+	implicit none
+	integer           :: i
+	type(linked_list) :: list
+	
+	do i = 1, 5
+		call prepend(list, i)
+	enddo
+	call display(list)
+
+	end program TestCVF
+
---
+++ mdl_linked_list.f90
@@ -0,0 +1,49 @@
+module mdl_linked_list
+	implicit none
+
+	type :: node
+		integer             :: value
+		type(node), pointer :: next => null()
+	end type node
+
+	type :: linked_list
+		type(node), pointer :: head => null()
+		type(node), pointer :: tail => null()
+	end type linked_list
+
+	contains
+		subroutine prepend(list, num)
+			implicit none
+			type(linked_list)   :: list
+			integer             :: num
+			type(node), pointer :: current => null()
+
+			if (.not. associated(list%head)) then
+				allocate(list%head)
+				list%head%value = num
+				list%tail => list%head
+			else
+				allocate(current)
+				current%value = num
+				current%next => list%head
+				list%head => current
+			endif
+			
+		end subroutine
+
+		subroutine display(list)
+			implicit none
+			type(linked_list)   :: list
+			type(node), pointer :: current
+
+			print *, '['
+			current => list%head
+			do
+				if (.not. associated(current)) exit
+				print *, current%value, ','
+				current => current%next
+			enddo
+			print *, ']'
+
+		end subroutine
+end module mdl_linked_list
